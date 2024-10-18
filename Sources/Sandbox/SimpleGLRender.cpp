8c979a885d57f3d5aec300fc5f93b095c08feca0
strelkovsky <strelkin7@gmail.com>
2024-10-18 11:59:59 +0300

    gl debug

--- Sources/Sandbox/SimpleGLRender.cpp
+++ Sources/Sandbox/SimpleGLRender.cpp
@@ -12,6 +12,7 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "Core/Base.h"
+#include "Platform/OpenGL/EugenixGL.h"
 #include "SimpleGLRender.h"
 
 #undef GL_OFFSET
@@ -19,48 +20,43 @@
 
 GLTexture::GLTexture(unsigned char* data, int width, int height, int comp)
 {
-	glGenTextures(1, &_glHandle);
-	glBindTexture(GL_TEXTURE_2D, _glHandle);
+	OPENGL_CALL(glGenTextures(1, &_glHandle));
+	OPENGL_CALL(glBindTexture(GL_TEXTURE_2D, _glHandle));
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	OPENGL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	OPENGL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	OPENGL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+	OPENGL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
 
-	if (comp == 3)
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	else if (comp == 4)
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	OPENGL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, comp == 3 ? GL_RGB : GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data));
 
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	
-	glBindTexture(GL_TEXTURE_2D, 0);
+	OPENGL_CALL(glGenerateMipmap(GL_TEXTURE_2D));
+	OPENGL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
 }
 
 GLTexture::~GLTexture()
 {
 	// TODO : check glIsBuffer
-	glDeleteTextures(1, &_glHandle);
+	//OPENGL_CALL(glDeleteTextures(1, &_glHandle));
 }
 
 void GLTexture::Use()
 {
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, _glHandle);
+	OPENGL_CALL(glActiveTexture(GL_TEXTURE0));
+	OPENGL_CALL(glBindTexture(GL_TEXTURE_2D, _glHandle));
 }
 
 void CheckShaderCompileStatus(GLHandle shaderId)
 {
 	GLint success;
-	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+	OPENGL_CALL(glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success));
 	if (!success)
 	{
 		GLint infoLogLength;
-		glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
+		OPENGL_CALL(glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLength));
 		std::vector<char> infoLog(std::max(infoLogLength, int(1)));
-		glGetShaderInfoLog(shaderId, infoLogLength, NULL, infoLog.data());
+		OPENGL_CALL(glGetShaderInfoLog(shaderId, infoLogLength, NULL, infoLog.data()));
 		Eugenix::Log::Error("Shader compilation failed: %s\n", infoLog);
 	}
 }
@@ -68,13 +64,13 @@ void CheckShaderCompileStatus(GLHandle shaderId)
 void CheckProgramLinkStatus(GLHandle programId)
 {
 	GLint success;
-	glGetProgramiv(programId, GL_LINK_STATUS, &success);
+	OPENGL_CALL(glGetProgramiv(programId, GL_LINK_STATUS, &success));
 	if (!success)
 	{
 		GLint infoLogLength;
-		glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &infoLogLength);
+		OPENGL_CALL(glGetProgramiv(programId, GL_INFO_LOG_LENGTH, &infoLogLength));
 		std::vector<char> infoLog(std::max(infoLogLength, int(1)));
-		glGetShaderInfoLog(programId, infoLogLength, NULL, infoLog.data());
+		OPENGL_CALL(glGetShaderInfoLog(programId, infoLogLength, NULL, infoLog.data()));
 		Eugenix::Log::Error("Program link failed: %s\n", infoLog);
 	}
 }
@@ -82,7 +78,7 @@ void CheckProgramLinkStatus(GLHandle programId)
 static std::map<std::string, GLHandle> GetUniformLocations(GLuint program)
 {
 	int numUni = -1;
-	glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &numUni);
+	OPENGL_CALL(glGetProgramiv(program, GL_ACTIVE_UNIFORMS, &numUni));
 
 	std::map<std::string, GLHandle> outUniforms;
 
@@ -93,7 +89,7 @@ static std::map<std::string, GLHandle> GetUniformLocations(GLuint program)
 		GLenum type;
 		char name[128];
 
-		glGetActiveUniform(program, static_cast<GLHandle>(i), sizeof(name) - 1, &namelen, &num, &type, name);
+		OPENGL_CALL(glGetActiveUniform(program, static_cast<GLHandle>(i), sizeof(name) - 1, &namelen, &num, &type, name));
 		name[namelen] = 0;
 		GLuint location = glGetUniformLocation(program, name);
 
@@ -109,24 +105,24 @@ GLShaderProgram::GLShaderProgram(const std::string& vsSource, const std::string&
 	const GLchar* vertexShaderRaw = vsSource.c_str();
 	const GLchar* fragmentShaderRaw = fsSource.c_str();
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderRaw, NULL);
-	glCompileShader(vertexShader);
+	GLuint vertexShader = OPENGL_CALL(glCreateShader(GL_VERTEX_SHADER));
+	OPENGL_CALL(glShaderSource(vertexShader, 1, &vertexShaderRaw, NULL));
+	OPENGL_CALL(glCompileShader(vertexShader));
 	CheckShaderCompileStatus(vertexShader);
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderRaw, NULL);
-	glCompileShader(fragmentShader);
+	OPENGL_CALL(glShaderSource(fragmentShader, 1, &fragmentShaderRaw, NULL));
+	OPENGL_CALL(glCompileShader(fragmentShader);)
 	CheckShaderCompileStatus(fragmentShader);
 
 	_glHandle = glCreateProgram();
-	glAttachShader(_glHandle, vertexShader);
-	glAttachShader(_glHandle, fragmentShader);
-	glLinkProgram(_glHandle);
+	OPENGL_CALL(glAttachShader(_glHandle, vertexShader));
+	OPENGL_CALL(glAttachShader(_glHandle, fragmentShader));
+	OPENGL_CALL(glLinkProgram(_glHandle));
 	CheckProgramLinkStatus(_glHandle);
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	OPENGL_CALL(glDeleteShader(vertexShader));
+	OPENGL_CALL(glDeleteShader(fragmentShader));
 
 	_uniformLocations = GetUniformLocations(_glHandle);
 }
@@ -134,24 +130,24 @@ GLShaderProgram::GLShaderProgram(const std::string& vsSource, const std::string&
 GLShaderProgram::~GLShaderProgram()
 {
 	// TODO : check glIsProgram
-	glDeleteProgram(_glHandle);
+	//OPENGL_CALL(glDeleteProgram(_glHandle));
 }
 
 void GLShaderProgram::Use()
 {
-	glUseProgram(_glHandle);
+	OPENGL_CALL(glUseProgram(_glHandle));
 }
 
 void GLShaderProgram::Unuse()
 {
-	glUseProgram(0);
+	OPENGL_CALL(glUseProgram(0));
 }
 
 void GLShaderProgram::SetUniform(const char* name, const glm::mat4& mat)
 {
 	//if (currentProgram != _glProgram)
 	//	Use();
-	glUniformMatrix4fv(_uniformLocations[name], 1, GL_FALSE, glm::value_ptr(mat));
+	OPENGL_CALL(glUniformMatrix4fv(_uniformLocations[name], 1, GL_FALSE, glm::value_ptr(mat)));
 }
 
 size_t GetAttribSize(StandartAttribs type)
@@ -174,19 +170,19 @@ GLenum GetAttribType(StandartAttribs type)
 
 void ToggleAttrib(StandartAttribs attrib, GLboolean normalized, size_t vertexSize, const GLvoid* offset)
 {
-	glEnableVertexAttribArray((GLuint)attrib);
-	glVertexAttribPointer((GLuint)attrib, GetAttribSize(attrib), GetAttribType(attrib), normalized, vertexSize, offset);
+	OPENGL_CALL(glEnableVertexAttribArray((GLuint)attrib));
+	OPENGL_CALL(glVertexAttribPointer((GLuint)attrib, GetAttribSize(attrib), GetAttribType(attrib), normalized, vertexSize, offset));
 }
 
 GLMesh::GLMesh(PrimitiveType type, uint32_t format, const void* vertData, size_t verticesSize, size_t vertexSize, const void* indData, size_t indiciesSize, size_t vertsCount, size_t indCount)
 	: _topology(type), _vertsCount(vertsCount), _indexCount(indCount)
 {
-	glGenVertexArrays(1, &_meshVao);
-	glBindVertexArray(_meshVao);
+	OPENGL_CALL(glGenVertexArrays(1, &_meshVao));
+	OPENGL_CALL(glBindVertexArray(_meshVao));
 
-	glGenBuffers(1, &_meshVbo);
-	glBindBuffer(GL_ARRAY_BUFFER, _meshVbo);
-	glBufferData(GL_ARRAY_BUFFER, verticesSize, vertData, GL_STATIC_DRAW);
+	OPENGL_CALL(glGenBuffers(1, &_meshVbo));
+	OPENGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, _meshVbo));
+	OPENGL_CALL(glBufferData(GL_ARRAY_BUFFER, verticesSize, vertData, GL_STATIC_DRAW));
 
 	if (format & VertexAttribs::Position)
 	{
@@ -199,9 +195,9 @@ GLMesh::GLMesh(PrimitiveType type, uint32_t format, const void* vertData, size_t
 
 	if (_indexCount > 0)
 	{
-		glGenBuffers(1, &_meshIbo);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _meshIbo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiciesSize, indData, GL_STATIC_DRAW);
+		OPENGL_CALL(glGenBuffers(1, &_meshIbo));
+		OPENGL_CALL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _meshIbo));
+		OPENGL_CALL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indiciesSize, indData, GL_STATIC_DRAW));
 	}
 }
 
@@ -209,26 +205,26 @@ GLMesh::~GLMesh()
 {
 	if (_meshVbo && glIsBuffer(_meshVbo))
 	{
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDeleteBuffers(1, &_meshVbo);
+		OPENGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		OPENGL_CALL(glDeleteBuffers(1, &_meshVbo));
 	}
 
 	if (_meshIbo && glIsBuffer(_meshIbo))
 	{
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDeleteBuffers(1, &_meshIbo);
+		OPENGL_CALL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+		OPENGL_CALL(glDeleteBuffers(1, &_meshIbo));
 	}
 
 	if (_meshVao && glIsVertexArray(_meshVao))
 	{
-		glBindVertexArray(0);
-		glDeleteVertexArrays(1, &_meshVao);
+		OPENGL_CALL(glBindVertexArray(0));
+		OPENGL_CALL(glDeleteVertexArrays(1, &_meshVao));
 	}
 }
 
 void GLMesh::Draw() const
 {
-	glBindVertexArray(_meshVao);
+	OPENGL_CALL(glBindVertexArray(_meshVao));
 
 	static const GLenum glPrimitive[] =
 	{
@@ -237,9 +233,13 @@ void GLMesh::Draw() const
 	};
 
 	if (_indexCount > 0)
-		glDrawElements(glPrimitive[(int)_topology], _indexCount, GL_UNSIGNED_INT, NULL);
+	{
+		OPENGL_CALL(glDrawElements(glPrimitive[(int)_topology], _indexCount, GL_UNSIGNED_INT, NULL));
+	}
 	else
-		glDrawArrays(glPrimitive[(int)_topology], 0, _vertsCount);
+	{
+		OPENGL_CALL(glDrawArrays(glPrimitive[(int)_topology], 0, _vertsCount));
+	}
 }
 
 namespace ShaderEnv
@@ -276,8 +276,8 @@ namespace ShaderEnv
 				})";
 
 				simpleDiffuseProgram = std::make_shared<GLShaderProgram>(vsSource, fsSource);
-				glBindAttribLocation(simpleDiffuseProgram->Id(), (GLuint)StandartAttribs::Position, "position");
-				glBindAttribLocation(simpleDiffuseProgram->Id(), (GLuint)StandartAttribs::Uv, "uv");
+				OPENGL_CALL(glBindAttribLocation(simpleDiffuseProgram->Id(), (GLuint)StandartAttribs::Position, "position"));
+				OPENGL_CALL(glBindAttribLocation(simpleDiffuseProgram->Id(), (GLuint)StandartAttribs::Uv, "uv"));
 			}
 			return simpleDiffuseProgram;
 		}
